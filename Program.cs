9257a9cb3bcbc6b3af0d3c17a6609c71ce2e368f
Kauã <kau.lira@yahoo.com>
2022-04-29 20:49:59 -0300

    Divisão finalizado

--- Program.cs
+++ Program.cs
@@ -35,7 +35,22 @@ switch (opcaoDesejada)
 
         break;
     case "-":
-        Console.WriteLine("Você selecionou subtração");
+        Console.WriteLine("--- Subtração A-B --- \n");
+        Console.WriteLine("Digite seus valores");
+
+        Console.Write("A = ");
+        a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("B = ");
+        b = Convert.ToDouble(Console.ReadLine());
+
+        resultado = a - b ;
+        
+        Console.WriteLine($"{a} - {b} = {resultado}");
+
+        Console.WriteLine("\nPressione uma tecla para continuar...");
+        Console.ReadKey();
+
         break;
     case "*":
         Console.WriteLine("--- Multiplicação A*B ---\n");
